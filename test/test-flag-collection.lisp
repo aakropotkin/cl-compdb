c556401a97d14010f2216ae5c3a1edb887e92e84
aakropotkin <alex.ameen.tx@gmail.com>
2021-07-11 16:25:21 -0500

    added test templates

---
+++ test/test-flag-collection.lisp
@@ -0,0 +1,16 @@
+
+(in-package :cl-user)
+(defpackage :compdb/test/test-flag-collection.lisp
+  (:USE :common-lisp :prove :compdb/flag-collection.lisp))
+(in-package :compdb/test/test-flag-collection.lisp)
+
+
+;; ========================================================================== ;;
+
+
+
+;; -------------------------------------------------------------------------- ;;
+
+
+
+;; ========================================================================== ;;
---
+++ test/test-struct-utils.lisp
@@ -0,0 +1,16 @@
+
+(in-package :cl-user)
+(defpackage :compdb/test/test-struct-utils.lisp
+  (:USE :common-lisp :prove :compdb/struct-utils.lisp))
+(in-package :compdb/test/test-struct-utils.lisp)
+
+
+;; ========================================================================== ;;
+
+
+
+;; -------------------------------------------------------------------------- ;;
+
+
+
+;; ========================================================================== ;;
