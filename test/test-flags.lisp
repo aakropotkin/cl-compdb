f80a09e28fd3e825d0ac39156219cee5a7ba983f
aakropotkin <alex.ameen.tx@gmail.com>
2021-07-11 20:19:19 -0500

    adding test for 'scoped-flag' struct.

--- test/test-flags.lisp
+++ test/test-flags.lisp
@@ -9,31 +9,30 @@
 ;; ========================================================================== ;;
 
 (subtest "Test `scoped-flag' struct."
-         (let ((sf1 (make-scoped-flag))
-               (sf2 (make-scoped-flag :FLAG "foo"))
-               (sf3 (make-scoped-flag :FLAG "bar"))
-               (sf4 (make-scoped-flag :FLAG "foo" :LOCAL T))
-               (sf5 (make-scoped-flag :FLAG "bar" :LOCAL T))
-               (sf6 (make-scoped-flag :FLAG "bar" :LOCAL T)))
+  (let ((sf1 (make-scoped-flag))
+        (sf2 (make-scoped-flag :FLAG "foo"))
+        (sf3 (make-scoped-flag :FLAG "bar"))
+        (sf4 (make-scoped-flag :FLAG "foo" :LOCAL T))
+        (sf5 (make-scoped-flag :FLAG "bar" :LOCAL T))
+        (sf6 (make-scoped-flag :FLAG "bar" :LOCAL T)))
 
-           (ok (scoped-flag-p sf1))
-           (ok (typep sf1 'scoped-flag))
+    (ok (scoped-flag-p sf1))
+    (ok (typep sf1 'scoped-flag))
 
-           (is (scoped-flag-flag sf1) ""    :TEST #'equal)
-           (is (scoped-flag-flag sf2) "foo" :TEST #'equal)
-           (is (scoped-flag-flag sf3) "bar" :TEST #'equal)
-           (is (scoped-flag-flag sf4) "foo" :TEST #'equal)
-           (is (scoped-flag-flag sf5) "bar" :TEST #'equal)
+    (is (scoped-flag-flag sf1) ""    :TEST #'equal)
+    (is (scoped-flag-flag sf2) "foo" :TEST #'equal)
+    (is (scoped-flag-flag sf3) "bar" :TEST #'equal)
+    (is (scoped-flag-flag sf4) "foo" :TEST #'equal)
+    (is (scoped-flag-flag sf5) "bar" :TEST #'equal)
 
-           (is (scoped-flag-local sf1) NIL)
-           (is (scoped-flag-local sf2) NIL)
-           (is (scoped-flag-local sf3) NIL)
-           (is (scoped-flag-local sf4) T)
-           (is (scoped-flag-local sf5) T)
+    (is (scoped-flag-local sf1) NIL)
+    (is (scoped-flag-local sf2) NIL)
+    (is (scoped-flag-local sf3) NIL)
+    (is (scoped-flag-local sf4) T)
+    (is (scoped-flag-local sf5) T)
 
-           (ok (not (equal sf5 sf6)))
-           (ok (equalp sf5 sf6))
-           ))
+    (ok (not (equal sf5 sf6)))
+    (ok (equalp sf5 sf6))))
 
 
 ;; -------------------------------------------------------------------------- ;;
