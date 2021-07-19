a973f7569e8c4096c301037776a9b26f3f108474
aakropotkin <alex.ameen.tx@gmail.com>
2021-07-19 18:06:39 -0500

    updating old 'test-flags.lisp' tests, without adding new ones

--- test/test-flags.lisp
+++ test/test-flags.lisp
@@ -4,16 +4,16 @@
 (defpackage :compdb/test/test-flags
   (:USE :common-lisp :prove :compdb/flags)
   (:EXPORT
-   #:test-flag-pair-t
-   #:test-flaggable-t
-   #:test-list-of-flags
+   #:test-raw-flag-pair-t
+   #:test-raw-flaggable-t
+   #:test-raw-list-of-flaggables
    #:run-test-flags))
 (in-package :compdb/test/test-flags)
 
 
 ;; ========================================================================== ;;
 
-(defun test-flag-pair-t ()
+(defun test-raw-flag-pair-t ()
     (subtest "Test `flag-pair' type."
       (ok (flag-pair-p (cons "foo" "bar")))
       (ok (flag-pair-p (cons "foo" #P"bar")))
@@ -29,30 +29,37 @@
 
 ;; -------------------------------------------------------------------------- ;;
 
-(defun test-flaggable-t ()
-    (subtest "Test `flaggable' type."
-      (ok (flaggable-p "foo"))
-      (ok (flaggable-p (cons "foo" "bar")))
-      (ok (flaggable-p (cons "foo" #P"bar")))
-      (ok (not (flaggable-p (cons "foo" 4))))
-      (ok (not (flaggable-p (cons #P"foo" 4))))
-      (ok (not (flaggable-p 4)))
-      (ok (not (flaggable-p 'foo)))
-      (is-type "foo" 'flaggable)
-      (is-type (cons "foo" "bar") 'flaggable)
-      (is-type (cons "foo" #P"bar") 'flaggable)
-      (ok (typep "foo" 'flaggable))
-      (ok (typep (cons "foo" "bar") 'flaggable))
-      (ok (typep (cons "foo" #P"bar") 'flaggable))
-      (ok (not (typep (cons "foo" 4) 'flaggable)))
-      (ok (not (typep (cons #P"foo" 4) 'flaggable)))
-      (ok (not (typep 4 'flaggable)))
-      (ok (not (typep 'foo 'flaggable)))))
+(defparameter *raw-flag-pairs*
+  (list (cons "foo" "bar")
+        (cons "foo" #P"bar")))
+
+(defparameter *not-raw-flag-pairs*
+  (list (cons #P"bar" "foo")
+        (cons "foo" 4)))
 
 
 ;; -------------------------------------------------------------------------- ;;
 
-(defun test-list-of-flaggables ()
+(defun test-raw-flaggable-t ()
+    (subtest "Test `flaggable' type."
+      (ok (flaggable-p "foo"))
+      (is-type "foo" 'flaggable)
+      (ok (typep "foo" 'flaggable))
+      (ok (every #'flaggable-p *raw-flag-pairs*))
+      (ok (notany #'flaggable-p *not-raw-flag-pairs*))
+      (ok (not (flaggable-p 4)))
+      (ok (not (typep 4 'flaggable)))
+      (ok (not (flaggable-p 'foo)))
+      (ok (not (typep 'foo 'flaggable)))
+      (is-type (cons "foo" "bar") 'flaggable)
+      (ok (typep (cons "foo" "bar") 'flaggable))
+      (is-type (cons "foo" #P"bar") 'flaggable)
+      (ok (typep (cons "foo" #P"bar") 'flaggable))))
+
+
+;; -------------------------------------------------------------------------- ;;
+
+(defun test-raw-list-of-flaggables ()
     (subtest "Test `list-of-flaggables' type."
       (ok (list-of-flaggables-p (list "foo" (cons "bar" "baz"))))
       (ok (list-of-flaggables-p (list)))
@@ -68,42 +75,10 @@
 
 ;; -------------------------------------------------------------------------- ;;
 
-;(defun test-scoped-flag ()
-;    (subtest "Test `scoped-flag' struct."
-;      (let ((sf1 (make-scoped-flag))
-;            (sf2 (make-scoped-flag :FLAG "foo"))
-;            (sf3 (make-scoped-flag :FLAG "bar"))
-;            (sf4 (make-scoped-flag :FLAG "foo" :LOCAL T))
-;            (sf5 (make-scoped-flag :FLAG "bar" :LOCAL T))
-;            (sf6 (make-scoped-flag :FLAG "bar" :LOCAL T)))
-;
-;        (ok (scoped-flag-p sf1))
-;        (ok (typep sf1 'scoped-flag))
-;
-;        (is (scoped-flag-flag sf1) ""    :TEST #'equal)
-;        (is (scoped-flag-flag sf2) "foo" :TEST #'equal)
-;        (is (scoped-flag-flag sf3) "bar" :TEST #'equal)
-;        (is (scoped-flag-flag sf4) "foo" :TEST #'equal)
-;        (is (scoped-flag-flag sf5) "bar" :TEST #'equal)
-;
-;        (is (scoped-flag-local sf1) NIL)
-;        (is (scoped-flag-local sf2) NIL)
-;        (is (scoped-flag-local sf3) NIL)
-;        (is (scoped-flag-local sf4) T)
-;        (is (scoped-flag-local sf5) T)
-;
-;        (ok (not (equal sf5 sf6)))
-;        (ok (equalp sf5 sf6)))))
-
-
-;; -------------------------------------------------------------------------- ;;
-
 (defun run-test-flags ()
-  (test-flag-pair-t)
-  (test-flaggable-t)
-  (test-list-of-flaggables)
-  ;(test-scoped-flag)
-  )
+  (test-raw-flag-pair-t)
+  (test-raw-flaggable-t)
+  (test-raw-list-of-flaggables))
 
 
 ;; -------------------------------------------------------------------------- ;;
